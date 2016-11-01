47e1d8e1ca3667a77e0d093361e0d422eb73117b
Joseph Miklus <joseph.miklus@gmail.com>
2016-10-31 20:41:03 -0400

    Initial commit -- barebones MVC app

---
+++ RaspberryMedbot/Controllers/ActivitiesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using RaspberryMedbot.Models;
+
+namespace RaspberryMedbot.Controllers
+{
+    public class ActivitiesController : Controller
+    {
+        private RaspberryMedbotContext db = new RaspberryMedbotContext();
+
+        // GET: Activities
+        public ActionResult Index()
+        {
+            var activities = db.Activities.Include(a => a.Patient);
+            return View(activities.ToList());
+        }
+
+        // GET: Activities/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Activity activity = db.Activities.Find(id);
+            if (activity == null)
+            {
+                return HttpNotFound();
+            }
+            return View(activity);
+        }
+
+        // GET: Activities/Create
+        public ActionResult Create()
+        {
+            ViewBag.PatientID = new SelectList(db.Patients, "PatientID", "FirstName");
+            return View();
+        }
+
+        // POST: Activities/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ActivityID,PatientID,Response,CreationTime,ResponseTime")] Activity activity)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Activities.Add(activity);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.PatientID = new SelectList(db.Patients, "PatientID", "FirstName", activity.PatientID);
+            return View(activity);
+        }
+
+        // GET: Activities/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Activity activity = db.Activities.Find(id);
+            if (activity == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.PatientID = new SelectList(db.Patients, "PatientID", "FirstName", activity.PatientID);
+            return View(activity);
+        }
+
+        // POST: Activities/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ActivityID,PatientID,Response,CreationTime,ResponseTime")] Activity activity)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(activity).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.PatientID = new SelectList(db.Patients, "PatientID", "FirstName", activity.PatientID);
+            return View(activity);
+        }
+
+        // GET: Activities/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Activity activity = db.Activities.Find(id);
+            if (activity == null)
+            {
+                return HttpNotFound();
+            }
+            return View(activity);
+        }
+
+        // POST: Activities/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Activity activity = db.Activities.Find(id);
+            db.Activities.Remove(activity);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ RaspberryMedbot/Models/Activity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RaspberryMedbot.Models
+{
+    public class Activity
+    {
+        [Key]
+        public int ActivityID { get; set; }
+        public int PatientID { get; set; }
+        public Patient Patient { get; set; }
+        public bool? Response { get; set; }
+        public DateTime CreationTime { get; set; }
+        public DateTime? ResponseTime { get; set; }
+    }
+}
